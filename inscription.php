aac6a3345c051b6a2548fd432b3de85fa9a1d898
Meiko Yahyaoui <meikotoulouse@gmail.com>
2025-08-04 13:49:55 +0200

    form inscription ,login

---
+++ inscription.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="username" placeholder="username" >
+        <input type="password" name="password" placeholder="passsword">
+        <button>Inscription</button>
+
+
+    </form>
+</body>
+</html>
